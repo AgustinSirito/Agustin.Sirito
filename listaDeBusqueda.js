03fc78d25d85dbaee5ebac75a709a512344ea6a2
agustinsirito <agus.sirito@gmail.com>
2023-08-09 20:52:11 -0300

    update varios

--- listaDeBusqueda.js
+++ listaDeBusqueda.js
@@ -1,5 +1,5 @@
 class Planta2 {
-    constructor( id, tipo, nombre, descripcion, precio, stock, size) {
+    constructor( id, tipo, nombre, descripcion, precio, stock, size, imagen) {
         this.id = id
         this.tipo = tipo;
         this.nombre = nombre;
@@ -8,12 +8,13 @@ class Planta2 {
         this.stock = stock;
         this.size = size;
         this.cantidad = 0;
+        this.imagen = imagen;
     }
 
     gethtml(){
         return(
         `<div class="card">` +
-        `<img class="cardImg" src="./img/favicon.gif" alt="Avatar">` +
+        `<img class="cardImg" src="${this.imagen}" alt="Avatar">` +
         `<div class="cardContainer">` +
         `<h4><b>${this.nombre}</b></h4>`+
         `<p><b>${this.descripcion}</b></p>`+
@@ -23,6 +24,7 @@ class Planta2 {
         `</div>`+
         `</div>`)
     }
+
 
     getResumenRowHtml(){
         return(
@@ -35,131 +37,139 @@ class Planta2 {
     }
 }
 
-
-//lectura de datos
-import productosDB from './productosDB.json' assert {type:"json"}
-
-const plantas = []
-
-for (let producto of productosDB.productos){
-    let planta = new Planta2(producto.id, producto.tipo, producto.nombre, producto.descripcion, producto.precio, producto.stock, producto.size)
-    plantas.push(planta)
+function renderizarListaProductos(){
+    listaDeProductos = ""
+    for(let planta of plantas){
+        listaDeProductos += planta.gethtml()
+    }
+    document.getElementById("listaDeBusqueda").innerHTML = listaDeProductos
 }
 
-let compras=[]
-let comprasLocalStorage=JSON.parse(localStorage.getItem("carrito"))
-if (comprasLocalStorage!=undefined ){
-    compras = comprasLocalStorage
-}
-
-actualizarCarrito()
-
-//array viejo
-// const plantas2 = [
-//     new Planta2(1, "planta", "Suculenta Haworthia", "Pequeña planta suculenta", 10.5, 5, "Pequeño"),
-//     new Planta2(2, "planta", "Helecho Nephrolepis exaltata", "Helecho colgante", 15.0, 4, "Mediano"),
-//     new Planta2(3, "planta", "Orquídea Phalaenopsis", "Orquídea de flores grandes", 20.75, 4, "Grande"),
-//     new Planta2(4, "planta", "Poto Epipremnum aureum", "Planta trepadora de hojas verdes", 12.99, 2, "Mediano"),
-//     new Planta2(5, "planta", "Cactus Echinocactus grusonii", "Cactus Barril", 25.5, 5, "Grande"),
-// ];
-
-//generacion de html listaDeProductos
-let listaDeProductos = ""
-for(let planta of plantas){
-    listaDeProductos += planta.gethtml()
+function actualizarListaComprasAnteriores(){
+    let comprasLocalStorage=JSON.parse(localStorage.getItem("carrito"))
+    if (comprasLocalStorage!=undefined ){
+        compras = comprasLocalStorage
+        actualizarCarrito()
+        document.getElementById("compras").classList.replace("hide", "show")
+    }
 }
 
 function comprar(){
-let precioFinal = 0;
-    for(let planta of plantas){
-        let cantidad = document.getElementById(`productPrice-${planta.id}`).value
-        planta.cantidad = cantidad
-        precioFinal += planta.precio * planta.cantidad
-    }
-    console.log("precio final: ", precioFinal)
-
-
-    let productosAComprar = plantas.filter((planta)=>{return(planta.cantidad!=0)})
-    
-
-    let listasDeProductosAComprarHtml = 
-    `<tr>
-    <th>Producto</th>
-    <th>Precio</th>
-    <th>Cantidad</th>
-    <th>Subtotal</th>
-    </tr>`
-
-
-    for(let planta of productosAComprar){
-        listasDeProductosAComprarHtml += planta.getResumenRowHtml()
-    }
-    listasDeProductosAComprarHtml += 
-    `<tr>`+
-    `<td></td>`+
-    `<td></td>`+
-    `<td>Subtotal: </td>`+
-    `<td>${Math.round(precioFinal * 100)/100} $ </td>`+
-    `</tr>`+
-    `<tr>`+
-    `<td></td>`+
-    `<td></td>`+
-    `<td>Precio final: </td>`+
-    `<td>${Math.round(precioFinal * 1.21 * 100)/100} $ </td>`+
-    `</tr>`
-
-
-    document.getElementById("resumenDeCompra").innerHTML = listasDeProductosAComprarHtml
-    document.getElementById("resumen").classList = ""
-    guardarDatosDeCompra(productosAComprar)
-}
-
-
-
-document.getElementById("listaDeBusqueda").innerHTML = listaDeProductos
-document.getElementById("botonComprar").onclick = comprar
-
-
-function guardarDatosDeCompra(productosDeCompra){
-    if(productosDeCompra.length!=0){
-        compras.push(productosDeCompra)
-        localStorage.setItem("carrito", JSON.stringify(compras));
-        actualizarCarrito()
-    }
-}
-
-function actualizarCarrito(){
-    let comprasAnterioresHTML= 
-    `<tr>
-    <th>Producto</th>
-    <th>Precio</th>
-    <th>Cantidad</th>
-    </tr>`
-
-
-    for(let compra of compras){
-        let precioFinalCompra = 0
-        for(let productoComprado of compra){
-            comprasAnterioresHTML += 
-            (
-                `<tr>`+
-                `<td><b>${productoComprado.nombre}</b></td>`+
-                `<td>${productoComprado.precio} $ </td>`+
-                `<td>${productoComprado.cantidad}</td>`+
-                `</tr>`
-            )
-            precioFinalCompra += productoComprado.precio * productoComprado.cantidad
+    let precioFinal = 0;
+        for(let planta of plantas){
+            let cantidad = document.getElementById(`productPrice-${planta.id}`).value
+            planta.cantidad = cantidad
+            precioFinal += planta.precio * planta.cantidad
         }
-
-        comprasAnterioresHTML+= 
+        // console.log("precio final: ", precioFinal)
+    
+        let productosAComprar = plantas.filter((planta)=>{return(planta.cantidad!=0)})
+    
+        let listasDeProductosAComprarHtml = 
+        `<tr>
+        <th>Producto</th>
+        <th>Precio</th>
+        <th>Cantidad</th>
+        <th>Subtotal</th>
+        </tr>`
+    
+        for(let planta of productosAComprar){
+            listasDeProductosAComprarHtml += planta.getResumenRowHtml()
+        }
+        listasDeProductosAComprarHtml += 
+        `<tr>`+
+        `<td></td>`+
+        `<td></td>`+
+        `<td>Subtotal: </td>`+
+        `<td>${Math.round(precioFinal * 100)/100} $ </td>`+
+        `</tr>`+
         `<tr>`+
         `<td></td>`+
         `<td></td>`+
         `<td>Precio final: </td>`+
-        `<td>${Math.round(precioFinalCompra * 1.21 * 100)/100} $ </td>`+
+        `<td>${Math.round(precioFinal * 1.21 * 100)/100} $ </td>`+
         `</tr>`
-        
+    
+    
+        document.getElementById("resumenDeCompra").innerHTML = listasDeProductosAComprarHtml
+        document.getElementById("resumen").classList = ""
+        guardarDatosDeCompra(productosAComprar)
+        actualizarListaComprasAnteriores()
+    
+        //reseteo de contadores
+        for(let planta of plantas){
+            planta.cantidad=0
+        }
+        renderizarListaProductos()
     }
 
-    document.getElementById("comprasAnteriores").innerHTML=comprasAnterioresHTML
-}
+    function guardarDatosDeCompra(productosDeCompra){
+        if(productosDeCompra.length!=0){
+            compras.push(productosDeCompra)
+            localStorage.setItem("carrito", JSON.stringify(compras));
+            actualizarCarrito()
+        }
+    }
+
+    function actualizarCarrito(){
+        let comprasAnterioresHTML = 
+        `<tr>
+        <th>Producto</th>
+        <th>Precio</th>
+        <th>Cantidad</th>
+        </tr>`
+        for(let compra of compras){
+            let precioFinalCompra = 0
+            for(let productoComprado of compra){
+                comprasAnterioresHTML += 
+                (
+                    `<tr>`+
+                    `<td><b>${productoComprado.nombre}</b></td>`+
+                    `<td>${productoComprado.precio} $ </td>`+
+                    `<td>${productoComprado.cantidad}</td>`+
+                    `</tr>`
+                )
+                precioFinalCompra += productoComprado.precio * productoComprado.cantidad
+            }
+            comprasAnterioresHTML+= 
+            `<tr>`+
+            `<td></td>`+
+            `<td></td>`+
+            `<td>Precio final: </td>`+
+            `<td>${Math.round(precioFinalCompra * 1.21 * 100)/100} $ </td>`+
+            `</tr>`
+            
+        }
+        document.getElementById("comprasAnteriores").innerHTML = comprasAnterioresHTML
+    }
+    
+    
+    function vaciar() {
+        localStorage.clear();
+        compras = []
+        actualizarCarrito()
+        document.getElementById("compras").classList = "hide"
+    
+    }
+
+
+//ejecucion
+let listaDeProductos = ""
+const plantas = []
+let compras=[]
+
+//lectura json via fetch
+fetch('./productosDB.json')
+.then(response => response.json())
+.then(productosDB => {
+    for (let producto of productosDB.productos){
+        let planta = new Planta2(producto.id, producto.tipo, producto.nombre, producto.descripcion, producto.precio, producto.stock, producto.size, producto.imagen)
+        plantas.push(planta)
+    }
+    renderizarListaProductos()
+})
+
+actualizarListaComprasAnteriores() //localstorage
+
+document.getElementById("botonComprar").onclick = comprar
+document.getElementById("botonVaciar").onclick = vaciar
